670c0235de2a0c7e4796d9bb69aaba10d95dcdea
Cyril MAGUIRE <github@ecyseo.net>
2014-01-14 03:14:53 +0100

    Update

---
+++ lang/fr.php
@@ -0,0 +1,9 @@
+<?php
+
+$LANG = array(
+	'L_CONFIG_DESCRIPTION'		=> 'Page de configuration du plugin inMyPluxml',
+	'L_CAT'						=> 'Mes liens',
+	'L_CAT_NAME'				=> 'Nom de la catégorie par défaut',
+	'L_CONFIG_SAVE'				=> 'Enregister'
+);
+?>
