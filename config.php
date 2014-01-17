348b08aefcfb8de445e28b7432bb7601eb27b745
Cyril MAGUIRE <github@ecyseo.net>
2014-01-18 00:30:23 +0100

    Add bookmarklet in config page

--- config.php
+++ config.php
@@ -33,3 +33,5 @@
 	<?php echo plxToken::getTokenPostMethod() ?>
 	<input type="submit" name="submit" value="<?php echo $plxPlugin->getLang('L_CONFIG_SAVE') ?>" />
 </form>
+<p>Glissez le bookmarklet suivant dans votre barre de favoris :</p>
+<p>Le bookmarklet -> <a href="javascript:javascript:(function(){var%20url%20=%20location.href;var%20title%20=%20document.title%20||%20url;window.open('<?php echo plxUtils::getRacine();?>core/admin/auth.php?p=article.php&amp;post='%20+%20encodeURIComponent(url)+'&amp;title='%20+%20encodeURIComponent(title)+'&amp;source=bookmarklet','_blank','menubar=no,height=800,width=800,toolbar=no,scrollbars=yes,status=no');})();">Pluxml</a></p>
